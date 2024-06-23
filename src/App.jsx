b085cf6607f66eaa3e01b51132c689f0954b233d
SalehRezaei2002 <amir10101webdev@gmail.com>
2024-06-23 22:38:40 +0330

    start project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>Vite and React logos to learn more</div>
+    </>
+  );
+}
+
+export default App;
