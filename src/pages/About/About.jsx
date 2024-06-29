76407df618028b722f63ff4b4efa3e4771427db6
SalehRezaei2002 <amir10101webdev@gmail.com>
2024-06-29 23:49:33 +0330

    set about me page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
 
-import './About.css'
+import "./About.css";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <div className="about-container">
+      <h1 className="my-name">AMIRSALEH REZAEI</h1>
+      <h2 className="questions">Who am I ?</h2>
+      <p className="desc">
+        I'm Amirsaleh Rezaei I'm 22 years old and study at Hakim sabzevari
+        university and Final year of bachelor's degree in computer engineering
+      </p>
+      <h2 className="questions">
+        What happened that I became a frontend developer ?
+      </h2>
+      <p className="desc">
+        When I entered university almost three years ago, I could say that I
+        didn't have any goals of what I wanted to do in university. It was at
+        the beginning of 1401 that this university became a full-time
+        university. And I found my interest. My interest was website design. I
+        have been working on the front end for 2 years and soon I will start
+        deep learning of backend and at the same time I am looking at learning
+        SEO.
+      </p>
+    </div>
+  );
 }
 
-export default About
+export default About;
